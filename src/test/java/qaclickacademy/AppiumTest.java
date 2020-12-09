4d10e174b9ab706ddb72341a8dca54a8b3a0f69e
Satish G <satishg@cybage.com>
2020-12-09 10:47:12 +0530

    my first github demo commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid form AppiumTest");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp test from AppiumTest");
+	}
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation test in SeleniumTest");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI test in SeleniumTest");
+		
+	}
+}
