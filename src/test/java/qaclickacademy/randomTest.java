39caa4e8da00fde8ec42b7006b1697f11f0ac1ba
Satish Ghorpade <satishg@cybage.com>
2021-02-22 10:34:37 +0530

    adding line for forth

--- src/test/java/qaclickacademy/randomTest.java
+++ src/test/java/qaclickacademy/randomTest.java
@@ -8,6 +8,7 @@ public class randomTest {
 		System.out.println("Testing script");
 		System.out.println("Adding line for demo");
 		System.out.println("Adding third line for demo");
+		System.out.println("Addin fourth line");
 	}
 
 }
